40fb4f6cd1c562f5bb94b9d6c5e92c4c3b42ba22
Stoic-Coder2003 <shubhmehrotra.personal@gmail.com>
2023-08-19 11:45:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const childContainer = document.getElementById("childContainer");
+
+function update() {
+    let date = new Date();
+
+    function formatTime(date) {
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let seconds = date.getSeconds();
+        let amOrpm = hours >= 12 ? "pm" : "am";
+
+        hours = (hours % 12) || 12;
+
+        hours = formatZeros(hours);
+        minutes = formatZeros(minutes);
+        seconds = formatZeros(seconds);
+
+        return `${hours}:${minutes}:${seconds} ${amOrpm}`;
+    }
+
+    function formatZeros(time) {
+        time = time.toString();
+        return time.length < 2 ? "0" + time : time;
+    }
+
+    const formattedTime = formatTime(date);
+    childContainer.textContent = formattedTime;
+}
+
+function updateWithDelay() {
+    update(); // Initial call to update immediately
+    setInterval(update, 1000); // Then update every second
+}
+
+updateWithDelay();
